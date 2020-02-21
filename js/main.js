80bceaea8a7e5f231af7c9c48bee46148592db6b
Marc Remolt <marc.remolt@gmail.com>
2020-02-21 14:40:04 +0100

    Added reading of user ID

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const usersElement = document.querySelector("#usersElement");
+const userIdElement = document.querySelector("#userIdElement");
 
+usersElement.addEventListener("outClickedItem", event => {
+  console.log(event.detail, "User");
+
+  // send the userId to the future user detail component here
+  userIdElement.innerHTML = event.detail.id;
+});
